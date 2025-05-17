9d557d7574122128f1955f7fa3494d7461d5893d
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-17 15:01:35 +0700

    include iostream

--- latihanSoalB.cpp
+++ latihanSoalB.cpp
@@ -1,4 +1,74 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class peminjam;
+class petugas;
+
+class buku{
+    private:
+        string judul;
+        string penulis;
+        bool dipinjam;
+
+    public:
+        buku(string j, string p) : judul(j), penulis(p), dipinjam(false){}
+        friend class petugas;
+};
+
+class peminjam{
+    private:
+        string nama;
+        int id;
+        int totalpinjaman;
+
+    public:
+        peminjam (string n, int i) : nama(n), id(i), totalpinjaman(0){}      
+};
+
+class petugas{
+    private:
+        string nama;
+        int idPetugas;
+        string levelAkses;
+
+    public:
+        petugas(string n, int id, string level) : nama(n), idPetugas(id), levelAkses(level) {}
+
+    void prosesPinjam(buku& b, peminjam& p) {
+        if (!b.dipinjam){
+            b.dipinjam = true;
+            p.totalpinjaman++;
+            cout << "buku berhasil dipinjam oleh " << p.nama << endl;
+        }
+    }else{
+        cout << "buku sedang dipinjam." << endl;
+    }
+}
+
+void proseskembali(buku b, peminjam p){
+    if (b.dipinjam){
+        b.dipinjam = false;
+        p.totalpinjaman--;
+        cout << "buku berhasil dikembalikkan oleh " << p.nama << endl;
+    }else{
+        cout << "buku tidak sedang dipinjam" << endl;
+    }
+};
+
+friend class admin;
+class admin{
+    public:
+        void ubahlevelAkses(petugas p, const string newLevel){
+            p.levelAkses = newLevel;
+            cout << "levelAkses petugas diubah menjadi: " << newLevel << endl;
+        }
+
+        void lihatStatistik(cosnt peminjam p, const buku b){
+            cout << "peminjam: " << p.nama << ", total pinjaman: " << p.totalpinjaman << endl;
+            cout << "status buku: " << b.dipinjam << "dipinjam" : "tersedia" << endl;
+        }
+};
 
 int main(){
     buku buku1("retorika");
