4429b94af2eda6506b3447109c5b80aaf3f6199b
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-17 14:29:12 +0700

    isi dari class buku

--- latihanSoalB.cpp
+++ latihanSoalB.cpp
@@ -1,5 +1,12 @@
 
 
-class peminjam;
-class petugas;
+class buku{
+    private:
+        string judul;
+        string penulis;
+        bool dipinjam;
 
+    public:
+        buku(string j, string p) : judul(j), penulis(p), dipinjam(false){}
+        friend class petugas;
+};
