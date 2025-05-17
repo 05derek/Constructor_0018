a8733a4422ee04f7bd7d1def8396b2849c8757f8
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-17 15:05:42 +0700

    isi class buku

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -1,4 +1,15 @@
 
 class buku{
+    string judul;
 
-}
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; //chain function
+    }
+    string getjudul;()
+    {
+        return this->judul;
+    }
+}bukunya;
