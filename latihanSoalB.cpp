7a9628341ffd1f67153ebb80a3c418a3d265d7cc
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-17 14:39:56 +0700

    proses peminjaman

--- latihanSoalB.cpp
+++ latihanSoalB.cpp
@@ -1,13 +1,12 @@
 
 
-class petugas{
-    private:
-        string nama;
-        int idPetugas;
-        string level akses;
-
-    public:
-        petuga(string n, int id, string level) : nama(n), idPetugas(id), levelAkses(level) {}
-
-
+    void prosesPinjam(buku& b, peminjam& p) {
+        if (!b.dipinjam){
+            b.dipinjam = true;
+            p.totalpinjaman++;
+            cout << "buku berhasil dipinjam oleh " << p.nama << endl;
+        }
+    }else{
+        cout << "buku sedang dipinjam." << endl;
+    }
 }
