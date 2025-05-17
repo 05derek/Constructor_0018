f5ae06d4d02373a2ee6179248dfc4f68ec832759
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-17 15:17:38 +0700

    destruktor terakhir

--- destruktor.cpp
+++ destruktor.cpp
@@ -1,8 +1,16 @@
 
 
-angka::angka(){
+void angka::isiData(){
+    for(int i=1; i<=panjang; i++){
+        cout<<i<<" = "; cin>>arr[i];
+    }
     cout<<endl;
-    cetakData();
-    delete[]arr;
-    cout << "Alamat Array sudah dilepaskan" << endl;
 }
+
+int main(){
+    angka belajarcpp(3);              // Constructor Dipanggil
+    angka *ptrBelajarcpp = new angka(5); // Constructor Dipanggil
+    delete ptrBelajarcpp;            // Destructor Dipanggil
+
+    return 0;
+} // Destructor Dipanggil
