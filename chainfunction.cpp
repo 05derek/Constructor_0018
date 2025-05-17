12796fded0ae117bd1e930d2d6620666cb2d8885
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-17 15:09:03 +0700

    int main

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -1,15 +1,8 @@
 
-class buku{
-    string judul;
 
-public:
-    buku setJudul(string judul)
-    {
-        this->judul = judul;
-        return *this; //chain function
-    }
-    string getjudul;()
-    {
-        return this->judul;
-    }
-}bukunya;
+int main()
+{
+    setjudul("matematika");
+    cout << bukunya.setjudul("matematika").getjudul();//chain function calss
+    return 0;
+}
