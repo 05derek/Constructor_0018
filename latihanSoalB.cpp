b54f01c6d1566a57bc84f5278f208c6a4b088a9d
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-17 14:45:03 +0700

    proses pengembalian buku part 1

--- latihanSoalB.cpp
+++ latihanSoalB.cpp
@@ -1,12 +1,9 @@
 
 
-    void prosesPinjam(buku& b, peminjam& p) {
-        if (!b.dipinjam){
-            b.dipinjam = true;
-            p.totalpinjaman++;
-            cout << "buku berhasil dipinjam oleh " << p.nama << endl;
-        }
-    }else{
-        cout << "buku sedang dipinjam." << endl;
+void proseskembali(buku& b, peminjam& p){
+    if (b.dipinjam){
+        b.dipinjam = false;
+        p.totalpinjaman--;
+        cout << "buku berhasil dikembalikkan oleh " << p.nama << endl;
     }
 }
