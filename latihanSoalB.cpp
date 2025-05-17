7e4d6ddcf2c83705859385ae1e60284d779b400e
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-17 14:54:27 +0700

    pembuatan class admin part 2

--- latihanSoalB.cpp
+++ latihanSoalB.cpp
@@ -1,9 +1,13 @@
 
-
-friend class admin;
 class admin{
     public:
         void ubahlevelAkses(petugas p, const string newLevel){
-
+            p.levelAkses = newLevel;
+            cout << "levelAkses petugas diubah menjadi: " << newLevel << endl;
         }
-}
+
+        void lihatStatistik(cosnt peminjam p, const buku b){
+            cout << "peminjam: " << p.nama << ", total pinjaman: " << p.totalpinjaman << endl;
+            cout << "status buku: " << b.dipinjam << "dipinjam" : "tersedia" << endl;
+        }
+};
